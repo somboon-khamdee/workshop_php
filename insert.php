e95f77af03abee47a0d6c5c330c0a81efe9b7bba
somboon-khamdee <somboon.khumdee@gmail.com>
2022-02-01 09:54:23 +0700

    Version 1.0

--- insert.php
+++ insert.php
@@ -3,9 +3,9 @@ $fullname = $_POST['text_fullname'];
 $username = $_POST['text_username'];
 $password = $_POST['text_password'];
 $repassword = $_POST['text_repassword'];
+
 echo $fullname."<br>";
 echo $username."<br>";
 echo $password."<br>";
 echo $repassword."<br>";
-
 ?>
